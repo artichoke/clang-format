aaca56d7ba86db3cde6650c35cc5718198c28f8d
Ryan Lopopolo <rjl@hyperbo.la>
2020-08-12 16:11:55 -0700

    Import clang-format script from artichoke/artichoke
    As of artichoke/artichoke@98c9f2921d4220a793b6b8cbe87252505b2f2385.

---
+++ cli.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+const path = require("path");
+const { program } = require("commander");
+const { STATUS, clangFormatter, walk } = require("./index");
+require("array-flat-polyfill");
+
+let formatDir = path.resolve(".");
+
+program
+  .version("0.1.0")
+  .arguments("[directory]")
+  .action((directory) => {
+    formatDir = directory;
+  })
+  .option("--check", "Run in 'check' mode.");
+
+program.parse(process.argv);
+
+(async function main() {
+  const timer = setInterval(() => {}, 100);
+  try {
+
+    const files = await walk(formatDir);
+    const lintState = await clangFormatter(files, program.check);
+    const failures = lintState
+      .flat(Infinity)
+      .filter((status) => status === STATUS.failed);
+    if (failures.length > 0) {
+      process.exit(1);
+    }
+  } catch (err) {
+    console.error("Error: Unhandled exception");
+    console.error(err);
+    process.exit(1);
+  } finally {
+    timer.unref();
+  }
+})();
