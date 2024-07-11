26bd20e49434ece686c08be092a0c9ca68fb49c4
Ryan Lopopolo <rjl@hyperbo.la>
2024-07-10 23:13:33 -0700

    Upgrade to eslint v9

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from "globals";
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ["dist/**/*", "**/vendor/**/*"],
+  },
+];
